52cfb4012a54ebb338ac20a35c53c416bd25f817
miguelangel.rodriguez.arevalo.contractor <miguelangel.rodriguez.arevalo.contractor@bbva.com>
2024-01-18 12:11:33 -0600

    Se agregan extencions para los la clase String

--- UtilitiesLib/src/main/java/com/bbva/utilitieslib/extensions/String.kt
+++ UtilitiesLib/src/main/java/com/bbva/utilitieslib/extensions/String.kt
@@ -1,20 +1,47 @@
 package com.bbva.utilitieslib.extensions
 
+fun String.toBoolean(): Boolean =
+    when (this) {
+        "0"  -> false
+        "1"  -> true
+        else -> throw IllegalArgumentException("ilegal value $this")
+    }
+
 private fun calcHexa(ch1: Char, ch2: Char) =
     ((ch1.toHexa() and 0xFF) shl 4 or (ch2.toHexa() and 0xFF)).toByte()
 
-fun String.toHexaByte(startIndex: Int = 0): Byte{
+fun String.toHexaByte(startIndex: Int = 0): Byte {
+
     if (this.isEmpty())
         return 0x00
 
-    if (startIndex > length - 1)
+    if (startIndex >= length - 1)
         throw IllegalArgumentException("startIndex [$startIndex] >= length -1 [$length-1]")
 
     return calcHexa(this[startIndex], this[startIndex + 1])
 }
 
-fun String.toHexaBytes(): ByteArray{
-    if(this.isEmpty())
+fun String.toHexaChar(startIndex: Int = 0): Char {
+    if (this.isEmpty())
+        return Char.MIN_VALUE
+
+    if (startIndex >= length - 1)
+        throw IllegalArgumentException("startIndex [$startIndex] >= length -1 [$length-1]")
+
+    return ((Character.digit(this[startIndex], 16) shl 4) + Character.digit(this[startIndex + 1], 16)).toChar()
+}
+
+fun String.toHexaAscii(): String {
+    var value = StringBuilder()
+    for (i in 0 until this.length step 2) {
+        value.append(this.toHexaChar(i))
+    }
+    return value.toString()
+}
+
+fun String.toHexaBytes(): ByteArray {
+
+    if (this.isEmpty())
         return byteArrayOf()
 
     val value = if (length.isOdd())
@@ -29,4 +56,22 @@ fun String.toHexaBytes(): ByteArray{
         result[pos++] = value.toHexaByte(index)
 
     return result
+}
+
+fun String.isNumber(): Boolean {
+    for (ch in this) {
+        if (!ch.isDigit())
+            return false
+    }
+    return true
+}
+
+fun String.substring(char: String, count: Int): String {
+    if (this.length < count)
+        return this
+
+    return if (this.contains(char))
+        this.substringAfter(char).substring(0, count)
+    else
+        this
 }
