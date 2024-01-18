ddec0be6501404608485c04f6e4fb7bef89cfa55
miguelangel.rodriguez.arevalo.contractor <miguelangel.rodriguez.arevalo.contractor@bbva.com>
2024-01-18 17:17:36 -0600

    Se agregan extencions para los la clase String

--- UtilitiesLib/src/main/java/com/bbva/utilitieslib/extensions/ByteArray.kt
+++ UtilitiesLib/src/main/java/com/bbva/utilitieslib/extensions/ByteArray.kt
@@ -7,3 +7,38 @@ fun ByteArray.toHexaString(): String {
     }
     return strBuilder.toString()
 }
+
+fun ByteArray.toHexBcd(): ByteArray {
+    require(size % 2 == 0) { "ByteArray length must be even." }
+
+    val result = ByteArray(size / 2)
+    var resultIndex = 0
+
+    for (i in indices step 2) {
+        val highNibble = Character.digit(this[i].toChar(), 16)
+        val lowNibble = Character.digit(this[i + 1].toChar(), 16)
+
+        result[resultIndex] = ((highNibble shl 4) or lowNibble).toByte()
+        resultIndex++
+    }
+
+    return result
+}
+
+fun ByteArray.readBcd(startIndex: Int, counter: Int): Int {
+    require(startIndex >= 0 && counter >= 0 && startIndex + counter <= size) {
+        "Invalid startIndex or counter for ByteArray readBCD operation."
+    }
+
+    var result = 0
+    var factor = 1
+
+    for (i in startIndex + counter - 1 downTo startIndex) {
+        val byteValue = this[i].toInt() and 0xFF
+        result += (byteValue % 10) * factor
+        result += ((byteValue / 10) % 10) * factor * 10
+        factor *= 100
+    }
+
+    return result
+}
--- UtilitiesLib/src/main/java/com/bbva/utilitieslib/extensions/Int.kt
+++ UtilitiesLib/src/main/java/com/bbva/utilitieslib/extensions/Int.kt
@@ -9,3 +9,31 @@ fun Int.toBoolean() = when (this) {
 fun Int.isOdd(): Boolean{
     return (this and 0x01).toBoolean()
 }
+
+fun Int.toBcd(): ByteArray {
+    require(this >= 0) { "Input value must be non-negative." }
+
+    val stringValue = toString()
+    val result = ByteArray((stringValue.length + 1) / 2)
+
+    var resultIndex = 0
+    var tempByte = 0
+    var shift = 0
+
+    for (i in stringValue.length - 1 downTo 0) {
+        val digit = Character.digit(stringValue[i], 10)
+        tempByte = tempByte or (digit shl shift)
+
+        if (shift == 4 || i == 0) {
+            result[resultIndex] = tempByte.toByte()
+            resultIndex++
+            tempByte = 0
+            shift = 0
+        } else {
+            shift += 4
+        }
+    }
+
+    result.reverse()
+    return result
+}
