b6513a86674c12a6b325c24807bb201be0f7d696
miguelangel.rodriguez.arevalo.contractor <miguelangel.rodriguez.arevalo.contractor@bbva.com>
2024-04-05 11:11:20 -0600

    Cambio minimo de sdk se coloca el 28

--- UtilitiesLib/src/main/java/com/bbva/utilitieslib/extensions/Context.kt
+++ UtilitiesLib/src/main/java/com/bbva/utilitieslib/extensions/Context.kt
@@ -1,6 +1,28 @@
 package com.bbva.utilitieslib.extensions
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.widget.Toast
 
 fun Context?.toast(text: String, duration: Int = Toast.LENGTH_SHORT) = this?.let { Toast.makeText(it, text, duration).show() }
+
+@SuppressLint("MissingPermission")
+fun Context.checkInternet(): Boolean {
+    val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+        return false
+    else {
+
+        val network = connectivityManager.activeNetwork ?: return false
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
+        }
+    }
+}
